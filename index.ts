c77200493653882bb216bb8018c0bc304d7833b6
saraelainee <sara.duarte@estudante.ifms.edu.br>
2025-05-12 17:20:38 -0300

    Inserimos novos atributos na tabela contemProdutos e funcionou!!!🎉🎀
    Co-authored-by: Vânia Moreira <vaniamrr@users.noreply.github.com>
    Co-authored-by: Lorena Mascarenhas <lorena.santos@estudante.ifms.edu.br>

--- index.ts
+++ index.ts
@@ -5,7 +5,11 @@ import cors from '@fastify/cors'
 const app = fastify()
 app.register(cors)
 
-//função get
+
+//----------------------------------------PARA A PAGINA CONTEMPRODUTOS.HTML ---------------------------------//
+
+// FUNÇÃO GET 
+
 app.get("/", (request: FastifyRequest, reply: FastifyReply) => {
     reply.send("Fastify Funcionando!")
 })
@@ -42,9 +46,10 @@ app.get("/contemProdutos.html", async (request: FastifyRequest, reply: FastifyRe
     
 })
 
-////função post
+//FUNÇÃO POST
+
 app.post("/contemProdutos", async (request: FastifyRequest, reply: FastifyReply) => {
-    const {nomeProdutosExistentes} = request.body as any
+    const {idExistentes, nomeProdutosExistentes, quantidadeExistentes, categoriaExistentes} = request.body as any
 
     //VERIFICAR SE O NOME É VAZIO
     try {
@@ -55,7 +60,7 @@ app.post("/contemProdutos", async (request: FastifyRequest, reply: FastifyReply)
           database: 'trabalho1Frameworks',
           port: 3306
       });
-        const resultado = await conn.query("INSERT INTO contemProdutos (nomeProdutosExistentes) VALUES (?)",[nomeProdutosExistentes])
+        const resultado = await conn.query("INSERT INTO contemProdutos (idExistentes, nomeProdutosExistentes, quantidadeExistentes, categoriaExistentes) VALUES (?,?,?,?)",[idExistentes,nomeProdutosExistentes,quantidadeExistentes,categoriaExistentes])
         const [dados,estruturaTabela] = resultado
         reply.send(dados)
 
@@ -74,6 +79,142 @@ app.post("/contemProdutos", async (request: FastifyRequest, reply: FastifyReply)
                 console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO");
                 reply.status(400).send({ mensagem: "ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO" });
                 break;
+            default:
+                console.log(erro);
+                reply.status(400).send({ mensagem: "ERRO DESCONHECIDO OLHE O TERMINAL DO BACKEND" });
+                break;
+        }
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------PARA A PAGINA PRODUTOSACOMPRA.HTML ---------------------------------//
+
+app.get("/:parametro", (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send("Fastify Funcionando!")
+})
+app.get("/produtosAComprar", async (request: FastifyRequest, reply: FastifyReply) => {
+   
+    try {
+        const conn1 = await mysql.createConnection( {
+          host: "localhost",
+          user: 'root',
+          password: "",
+          database: 'trabalho1Frameworks',
+          port: 3306
+      });
+        const resultado = await conn1.query("SELECT * FROM produtosAComprar")
+        const [dados,estruturaTabela] = resultado
+        reply.send(dados)
+
+//Verificação de erros
+    } catch (erro:any) {
+        if (erro.code === "ECONNREFUSED") {
+            console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!")
+            reply.status(400).send({mensagem:"ERRO: LIGUE O LARAGÃO!!! CABEÇA!"})
+        } else if (erro.code === "ER_BAD_DB_ERROR") {
+            console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO")
+            reply.status(400).send({mensagem:"ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO"})
+        } else if (erro.code === "ER_ACCESS_DENIED_ERROR") {
+            console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO")
+            reply.status(400).send({mensagem:"ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO"})
+        } else {
+            console.log(erro)
+            reply.status(400).send({mensagem:"ERRO DESCONHECIDO OLHE O TERMINAL"})
+        }
+    }
+})
+
+
+
+//FUNÇÃO POST
+app.post("/produtosAComprar", async (request: FastifyRequest, reply: FastifyReply) => {
+    const {nomeProdutosFaltantes} = request.body as any
+
+    //VERIFICAR SE O NOME É VAZIO
+    try {
+        const conn1 = await mysql.createConnection( {
+          host: "localhost",
+          user: 'root',
+          password: "",
+          database: 'trabalho1Frameworks',
+          port: 3306
+      });
+        const resultado1 = await conn1.query("INSERT INTO produtosAComprar (nomeProdutosFaltantes) VALUES (?)",[nomeProdutosFaltantes])
+        const [dados1,estruturaTabela1] = resultado1
+        reply.send(dados1)
+
+//Verificação de erros
+    } catch (erro:any) {
+        switch (erro.code) {
+            case "ECONNREFUSED":
+                console.log("ERRO: LIGUE O LARAGON");
+                reply.status(400).send({ mensagem: "ERRO: LIGUE O LARAGON" });
+                break;
+            case "ER_BAD_DB_ERROR":
+                console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O NOME DO BANCO DE DADOS" });
+                break;
+            case "ER_ACCESS_DENIED_ERROR":
+                console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO" });
+                break;
             case "ER_DUP_ENTRY":
                 console.log("ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA");
                 reply.status(400).send({ mensagem: "ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA" });
@@ -87,7 +228,7 @@ app.post("/contemProdutos", async (request: FastifyRequest, reply: FastifyReply)
     
 })
 
-
+//////////////////////////////////////////////////////////////////////////////////
 app.listen({ port: 8001 }, (erro, endereco) => {
     if (erro) {
         console.log("ERRO: Fastify não iniciou")
--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-//Contem produtos
+//------------------------------------------------------Produtos Existentes-----------------------------------------------------//
+async function buscaDados() {
+            const resposta = await fetch("http://localhost:8001/contemProdutos.html")
+            console.log(resposta)
+            if (resposta.status === 200) {
+                const dados = await resposta.json()
+                console.log(dados)
+                const ul = document.getElementById("minhalistaContem")
+                for (let i = 0; i < dados.length; i++) {
+
+                    const obj = dados[i];
+
+                    const li = document.createElement("li")
+
+                    li.innerText = `${obj.idExistentes}. ${obj.nomeProdutosExistentes} - ${obj.quantidadeExistentes} - ${obj.categoriaExistentes}`
+
+                    console.log(obj.nomeProdutosExistentes)
+
+                    ul.appendChild(li)
+
+                }
+
+
+            } else {
+                if (resposta.status === 400) {
+                    const dados = await resposta.json()
+                    alert(dados.mensagem)
+                    console.log(obj.contemProdutos)
+                } else {
+                    console.log("Erro Desconhecido!")
+                }
+            }
+
+        }
+        buscaDados()
+
+
+//------------------------------------------------------Produtos Existentes-----------------------------------------------------//
 async function trataForm() {
+            const idExistentesInput = document.getElementById("idExistentes")
             const contemProdutosInput = document.getElementById("nomeProdutosExistentes")
+            const quantidadeProdutosInput = document.getElementById("quantidadeExistentes")
+            const categoriaProdutosInput = document.getElementById("categoriaExistentes")
+
+            const idExistentes = idExistentesInput.value
             const nomeProdutosExistentes = contemProdutosInput.value
+            const quantidadeExistentes = quantidadeProdutosInput.value
+            const categoriaExistentes = categoriaProdutosInput.value
 
             const objcontemProdutos = {
-                nomeProdutosExistentes
+                idExistentes,
+                nomeProdutosExistentes,
+                quantidadeExistentes,
+                categoriaExistentes
             }
             try {
                 const resposta = await fetch("http://localhost:8001/contemProdutos", {
@@ -31,28 +78,68 @@ async function trataForm() {
             }
         }
 
-//------------------------------------------------------Produtos a comprar-----------------------------------------------------//
-async function trataForm() {
-            const contemProdutosInput = document.getElementById("nomeProdutosAComprar")
-            const nomeProdutosAComprar = contemProdutosInput.value
+    
+        
 
-            const objcontemProdutos = {
-                nomeProdutosExistentes
+
+    
+//------------------------------------------------------Produtos a comprar-----------------------------------------------------//
+async function buscaDadosComprar() {
+            const resposta1 = await fetch("http://localhost:8001/produtosAComprar")
+            console.log(resposta1)
+            if (resposta1.status === 200) {
+                const dados1 = await resposta1.json()
+                console.log(dados1)
+                const ul = document.getElementById("minhalistaFaltantes")
+                for (let i = 0; i < dados1.length; i++) {
+
+                    const obj = dados1[i];
+
+                    const li = document.createElement("li")
+
+                    li.innerText = `${obj.nomeProdutosFaltantes}`
+
+                    console.log(obj.nomeProdutosFaltantes)
+
+                    ul.appendChild(li)
+
+                }
+
+
+            } else {
+                if (resposta1.status === 400) {
+                    const dados1 = await resposta.json()
+                    alert(dados1.mensagem)
+                    console.log(obj.produtosAComprar)
+                } else {
+                    console.log("Erro Desconhecido!")
+                }
+            }
+
+        }
+        buscaDadosComprar()
+
+async function trataFormFalta() {
+            const produtosAComprarInput = document.getElementById("produtosAComprar")
+            const nomeProdutosFaltantes = produtosAComprarInput.value
+
+            const objprodutosAComprar = {
+                nomeProdutosFaltantes
             }
             try {
-                const resposta = await fetch("http://localhost:8001/contemProdutos", {
+                const resposta = await fetch("http://localhost:8001/produtosAComprar", {
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     method: "POST",
-                    body: JSON.stringify(objcontemProdutos)
+                    body: JSON.stringify(objprodutosAComprar)
                 })
                 if(resposta.status===404){
                     console.log("Produto não foi excluído")
                 }
                 if(resposta.status===400){
-                    const dados = await resposta.json()
-                    alert(`Deu erro do lado do servidor\n" ${dados.mensagem}`)
+                    const dados1 = await resposta.json()
+                    alert(`Deu erro do lado do servidor\n" ${dados1.mensagem}`)
                 }
                 if(resposta.status===200){
                     alert("Produto cadastrado com sucesso")
